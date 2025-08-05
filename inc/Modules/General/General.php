5fc9d08d73cb51920e2a1b43197974a8c768907f
Sam Sawyer <sawyer@americamedia.org>
2025-08-05 16:45:18 -0400

    Updating Yoast filter names due to deprecation of first versions

--- inc/Modules/General/General.php
+++ inc/Modules/General/General.php
@@ -22,13 +22,13 @@ class General {
 	public static function init() {
 
 		// Disable Yoast monitoring of slug changes for redirect; Redirection plugin takes care of this
-		add_filter( 'wpseo_premium_post_redirect_slug_change', '__return_true' );
+		add_filter( 'Yoast\WP\SEO\post_redirect_slug_change', '__return_true' );
 		// Also disable notices about Yoast redirects
-		add_filter( 'wpseo_enable_notification_post_trash', '__return_false' );
-		add_filter( 'wpseo_enable_notification_post_slug_change', '__return_false' );
+		add_filter( 'Yoast\WP\SEO\enable_notification_post_trash', '__return_false' );
+		add_filter( 'Yoast\WP\SEO\enable_notification_post_slug_change', '__return_false' );
 
 		// This would disable Yoast montioring of taxonomy changes for redirect — 
 		// but redirection plugin doesn't do this, so leave it active.
-		// add_filter( 'wpseo_premium_term_redirect_slug_change', '__return_true' );
+		// add_filter( 'Yoast\WP\SEO\term_redirect_slug_change', '__return_true' );
 	}
 }
